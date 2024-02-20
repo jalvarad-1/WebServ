dd1a139f23e04acf21d6c69e9fe1a715d7008259
Gutten42 <victorgutten@gmail.com>
2024-02-20 18:02:10 +0100

    multiserver cleaning

--- src/server/MultiServer.hpp
+++ src/server/MultiServer.hpp
@@ -14,13 +14,6 @@
 #define CONNECTION_SOCKET 1
 #define CGI_FD 2
 
-struct fd_status{
-    // std::chrono::system_clock::time_point fecha;
-    int status;
-    bool port;
-    HTTPServer* server;
-};
-
 struct fd_info{// 
     short int fd_type;//listening socket / connection  / cgi_fd
     HTTPServer* server;
@@ -31,16 +24,12 @@ class MultiServer
     private:
         // std::vector<HTTPServer*> servers;
         std::vector<struct pollfd> poll_fds;
-        std::vector<struct fd_status> status;
-		std::vector<HTTPServer *> waifu;
 		std::map<int,fd_info> fd_index;
 
     public:
         /// TODO to decide type of variable to pass to Constructor
         std::vector<struct pollfd> get_fds();
-        std::vector<struct fd_status> get_status();
         void set_fds(struct pollfd);
-        void set_status(struct fd_status);
         MultiServer(const std::vector<ServerConfig>& serverConfigs);
         ~MultiServer();
         void run();

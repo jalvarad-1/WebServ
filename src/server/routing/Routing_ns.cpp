866e73a2100173de1cbce92c333a71fdf937c52e
jalvarad-1 <josealvaradocevallos@gmail.com>
2024-03-01 20:26:13 +0100

    static resourse olyy on get requests

--- src/server/routing/Routing_ns.cpp
+++ src/server/routing/Routing_ns.cpp
@@ -36,7 +36,7 @@ std::string Routing::removeKeyValue(std::string toRemove, std::string str)
     return str.substr(pos + toRemove.length());
 }
 
-short int Routing::typeOfResource(const std::string& path, LocationRules locationRule)
+short int Routing::typeOfResource(const std::string& path, LocationRules locationRule, std::string method)
 {
     struct stat statbuf;
 
@@ -45,12 +45,14 @@ short int Routing::typeOfResource(const std::string& path, LocationRules locatio
         return ISCGI;
     }
     if (stat(path.c_str(), &statbuf) != 0)
-        return false;
+        return ISNF;
+    else if (method != "GET")
+        return ISNAM; 
     else if (S_ISREG(statbuf.st_mode))
         return ISFILE; // Verifica si es un archivo regular
     else if (S_ISDIR(statbuf.st_mode))
         return ISDIR;
-    return false;
+    return ISNAM;
 }
 
 Response Routing::processFilePath(std::string resource_path)
--- src/server/routing/Routing_ns.hpp
+++ src/server/routing/Routing_ns.hpp
@@ -10,10 +10,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-
+#define ISNF 0
 #define ISDIR 1
 #define ISFILE 2
 #define ISCGI 3
+#define ISNAM 4 // Not allowed Method
 
 class HTTPRequest;
 namespace Routing
@@ -21,7 +22,7 @@ namespace Routing
     LocationRules  & determineResourceLocation(ServerConfig &serverConfig, HTTPRequest httpRequest);//1
     bool            isAllowedMethod(const std::string & method, const std::list<std::string> & allowed_methods);//3
     std::string     removeKeyValue(std::string toRemove, std::string str);//3
-    short int       typeOfResource(const std::string& path, LocationRules locationRule);//3
+    short int       typeOfResource(const std::string& path, LocationRules locationRule, std::string method);//3
 
     Response        processDirPath(std::string file_path, LocationRules locationRule);//3
     Response        processFilePath(std::string resource_path);//3
